b2c5a4f9b9586149ccb3dc81e9dae3565c7f0d68
David <dcc.gm.2007@gmail.com>
2026-05-05 15:48:42 +0200

    Añadido componente contact

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
-export class Contact {}
+export class Contact {
+  // Lógica del componente
+}
