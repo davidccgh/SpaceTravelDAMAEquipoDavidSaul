47dd2a330fe151173a58840b5ee788b2b4e617f1
Saul <saulalonsocaballero@gmail.com>
2026-05-05 13:52:15 +0200

    componente stages creado

--- src/app/components/stages/stages.ts
+++ src/app/components/stages/stages.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-stages',
+  standalone: true,
   imports: [],
   templateUrl: './stages.html',
   styleUrl: './stages.css',
 })
-export class Stages {}
+export class Stages {
+  // No se requiere lógica adicional por ahora
+}
